43ec86a624a3634ac96d27296dbb54e4efe14659
Cihad Cengiz <ccengizstu@gmail.com>
2021-10-18 22:22:17 +0300

    express.static

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(buildPath));
+// app.use(express.static(buildPath));
 // app.use('/static', express.static(buildPath));
 app.use(express.static(__dirname + '/client/build'));
 
