fd4c15c903923b0a8baccece1fa21457b3e4153d
Cihad Cengiz <ccengizstu@gmail.com>
2021-10-18 12:31:04 +0300

    index.js path problems

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(buildPath));
-app.use('/static', express.static(buildPath));
+// app.use('/static', express.static(buildPath));
 
 sequelize.sync().then(() => console.log("db is ready"));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 app.get("/jobs", async (req, res) => {
