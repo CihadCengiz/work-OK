5e13a00f53ac5daa9764168fe61892d4734be01e
Cihad Cengiz <ccengizstu@gmail.com>
2021-10-23 17:04:01 +0300

    changed path again
    im about to go crazy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 const creds = require('./contact/config');
 const path = require('path');
-const buildPath = path.join(__dirname, 'client', 'build');
+const buildPath = path.join(__dirname + '/client/build');
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 3001;
