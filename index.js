11eec1e830bf10a536e15365fb8bf0f0d4ffd199
Cihad Cengiz <ccengizstu@gmail.com>
2021-11-06 17:38:29 +0300

    color theme and small details

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(buildPath));
-
 sequelize.sync().then(() => console.log("db is ready"));
 
 
